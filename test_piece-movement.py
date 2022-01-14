eda77f667bd603db46ff85fbeeb43b89f006c279
changethisname2 <61044612+changethisname2@users.noreply.github.com>
2022-01-14 19:23:30 +0800

    Update and rename test_piece-movement to test_piece-movement.py

--- test_piece-movement.py
+++ test_piece-movement.py
@@ -1,6 +1,7 @@
 def grab_piece(x, y):
     above_piece, rx', ry', rz' =-0.1502, -2.1890, 2.1920,=-0.0111
     at_piece, rx, ry, rz = -0.2025, -2.1890,2.1910,-0.0110
+    gripper.open_gripper()
     rob.movel([x, y, above_piece, rx', ry', rz'],a,v)
     rob.movel([x, y, at_piece, rx, ry, rz],a,v)
     gripper.close_gripper()
