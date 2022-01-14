5c60a4c7bb15fd5ada15415c3df2d40b3bd9c057
ktjq-2605 <97417544+ktjq-2605@users.noreply.github.com>
2022-01-14 17:35:10 +0800

    Update test_blobdetection(14-1-2022).py

--- test_blobdetection(14-1-2022).py
+++ test_blobdetection(14-1-2022).py
@@ -6,17 +6,8 @@ import cv2
 import sys
 import numpy as np
 
-image = cv2.imread("opencv_frame_0.png")
+image = cv2.imread("pre-game board.png")
 
-
-h,w,c = image.shape
-ratio = int(h) / int(w)
-
-# let's downscale the image using new  width and height
-down_width = 600
-down_height = int(down_width * ratio)
-down_points = (down_width, down_height)
-resized_down = cv2.resize(image, down_points, interpolation=cv2.INTER_LINEAR)
 
 ''' aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 parameters = cv2.aruco.DetectorParameters_create()
@@ -71,53 +62,41 @@ else :
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-h,w = img_gray.shape
-ratio = int(h) / int(w)
-
-# let's upscale the image using new  width and height
-up_width = 600
-up_height = int(up_width * ratio)
-up_points = (up_width, up_height)
-resized_up = cv2.resize(img_gray, up_points, interpolation=cv2.INTER_LINEAR)
-cv2.waitKey(0)
-
-
-h,w,c = image.shape
-
 
 # Detect blobs.
-keypoints = detector.detect(resized_up)
+keypoints = detector.detect(img_gray)
 
 cordinate = []
 
-
+number = 0
 for keypoint in keypoints:
     pixelX = int(keypoint.pt[0])
     pixelY = int(keypoint.pt[1])
-    mXcv = pixelX * 0.000264583333
-    mYcv = pixelY * 0.000264583333
+    mXcv = pixelX * 0.000826086957
+    mYcv = pixelY * 0.000826086957
     mXcv = float("{:.4f}".format(mXcv))
     mYcv = float("{:.4f}".format(mYcv))
-    mXurx = 0.8828 - mYcv
-    mYurx = 0.0450 - mXcv
-    cordTup = (mXcv,mYcv)
+    mXurx = 0.89517 - mYcv
+    mYurx = 0.22763 - mXcv
+    cordTup = (mXurx,mYurx)
     cordinate.append(cordTup)
-    print (cordTup)
-#print (cordinate)
+    number += 1
+    print (number, cordTup)
 
-imageCircle = resized_up.copy()
-circle_center = (29, 31)
+imageCircle = img_gray.copy()
+circle_center1 = (405, 194)
 radius = 10
-cv2.circle(imageCircle, circle_center, radius, (255, 255, 255), 3, cv2.LINE_AA)
+cv2.circle(imageCircle, circle_center1, radius, (255, 255, 255), 3, cv2.LINE_AA) #white
 cv2.imshow("image Circle", imageCircle)
 cv2.waitKey(0)
 
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(resized_up, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+im_with_keypoints = cv2.drawKeypoints(img_gray, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show keypoints
 cv2.imshow("Keypoints", im_with_keypoints)
 cv2.waitKey(0)
+
 
 cv2.destroyAllWindows()
