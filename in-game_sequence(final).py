a0129235f3b42315ca8bb517e0752235ba7aa642
ktjq-2605 <97417544+ktjq-2605@users.noreply.github.com>
2022-01-15 23:30:22 +0800

    Update in-game_sequence(final).py

--- in-game_sequence(final).py
+++ in-game_sequence(final).py
@@ -9,6 +9,7 @@ import imutils
 import sys
 import numpy as np
 from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
+
 def grab_piece(x, y):
     above_piece, rx', ry', rz' =-0.1502, -2.1890, 2.1920,=-0.0111
     at_piece, rx, ry, rz = -0.2025, -2.1890,2.1910,-0.0110
@@ -21,8 +22,6 @@ def place_piece(x, y):
     rob.movel([x, y, at_piece, rx, ry, rz],a,v)
     gripper.open_gripper()
     rob.movel([x, y, above_piece, rx', ry', rz'],a,v)
-
-
 
 if __name__ == "__main__":
     try:
