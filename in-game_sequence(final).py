cdac4e2f84a05010ca1928ffe836ed328611c5e4
ktjq-2605 <97417544+ktjq-2605@users.noreply.github.com>
2022-01-16 04:10:33 +0800

    Update in-game_sequence(final).py

--- in-game_sequence(final).py
+++ in-game_sequence(final).py
@@ -12,17 +12,17 @@ from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
 
 a, v = 0.5, 0.3
 def grab_piece(x, y):
-    above_piece, rx', ry', rz' = -0.1502, -2.1890, 2.1920, -0.0111
+    above_piece, rxb, ryb, rzb = -0.1502, -2.1890, 2.1920, -0.0111
     at_piece, rx, ry, rz = -0.2025, -2.1890, 2.1910, -0.0110
-    rob.movel([x, y, above_piece, rx', ry', rz'], a, v)
+    rob.movel([x, y, above_piece, rxb, ryb, rzb], a, v)
     rob.movel([x, y, at_piece, rx, ry, rz], a, v)
     gripper.close_gripper()
     rob.movel([x, y, above_piece, rx, ry, rz], a, v)
 def place_piece(x, y):
-    rob.movel([x, y, above_piece, rx', ry', rz'], a, v)
+    rob.movel([x, y, above_piece, rxb, ryb, rzb], a, v)
     rob.movel([x, y, at_piece, rx, ry, rz],a,v)
     gripper.open_gripper()
-    rob.movel([x, y, above_piece, rx', ry', rz'], a, v)
+    rob.movel([x, y, above_piece, rxb, ryb, rzb], a, v)
 
 if __name__ == "__main__":
     try:
