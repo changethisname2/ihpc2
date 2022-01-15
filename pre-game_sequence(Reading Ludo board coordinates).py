5546c4fba9d943dce15341b0ce26258f07be0ad6
ktjq-2605 <97417544+ktjq-2605@users.noreply.github.com>
2022-01-16 04:11:52 +0800

    Update pre-game_sequence(Reading Ludo board coordinates).py

--- pre-game_sequence(Reading Ludo board coordinates).py
+++ pre-game_sequence(Reading Ludo board coordinates).py
@@ -14,22 +14,23 @@ pos_dice_drop = [0.707, 0.219, 0.030, -2.205, 2.205, -0.043]
 pos_dice_grab = [0.719, 0.219, -0.200, -2.205, 2.205, -0.043]
 pos_board = [0.726, -0.078, 0.085, -2.125, 2.190, -0.108]
 
+a, v = 0.5, 0.3
 def grab_piece(x, y):
     above_piece, rxb, ryb, rzb = -0.1502, -2.1890, 2.1920, -0.0111
     at_piece, rx, ry, rz = -0.2025, -2.1890,2.1910,-0.0110
     gripper.open_gripper()
-    rob.movel([x, y, above_piece, rxb, ryb, rzb],0.2,0.3)
-    rob.movel([x, y, at_piece, rx, ry, rz],0.2,0.3)
+    rob.movel([x, y, above_piece, rxb, ryb, rzb], a, v)
+    rob.movel([x, y, at_piece, rx, ry, rz], a, v)
     gripper.close_gripper()
-    rob.movel([x, y, above_piece, rx, ry, rz],0.2,0.3)
+    rob.movel([x, y, above_piece, rx, ry, rz], a, v)
 
 def place_piece(x, y):
     above_piece, rxb, ryb, rzb = -0.1502, -2.1890, 2.1920, -0.0111
     at_piece, rx, ry, rz = -0.2025, -2.1890,2.1910,-0.0110
-    rob.movel([x, y, above_piece, rxb, ryb, rzb],0.2,0.3)
-    rob.movel([x, y, at_piece, rx, ry, rz],0.2,0.3)
+    rob.movel([x, y, above_piece, rxb, ryb, rzb], a, v)
+    rob.movel([x, y, at_piece, rx, ry, rz], a, v)
     gripper.open_gripper()
-    rob.movel([x, y, above_piece, rxb, ryb, rzb],0.2,0.3)
+    rob.movel([x, y, above_piece, rxb, ryb, rzb], a, v)
 
 if __name__ == "__main__":
     try:
