201fbf74fcdb86aa777fe48a3d786d96418280eb
llj1125 <97417545+llj1125@users.noreply.github.com>
2022-01-15 10:34:26 +0800

    Update in-game_sequence(final).py

--- in-game_sequence(final).py
+++ in-game_sequence(final).py
@@ -9,6 +9,19 @@ import imutils
 import sys
 import numpy as np
 from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
+def grab_piece(x, y):
+    above_piece, rx', ry', rz' =-0.1502, -2.1890, 2.1920,=-0.0111
+    at_piece, rx, ry, rz = -0.2025, -2.1890,2.1910,-0.0110
+    rob.movel([x, y, above_piece, rx', ry', rz'],a,v)
+    rob.movel([x, y, at_piece, rx, ry, rz],a,v)
+    gripper.close_gripper()
+    rob.movel([x, y, above_piece, rx, ry, rz],a,v)
+def place_piece(x, y):
+    rob.movel([x, y, above_piece, rx', ry', rz'],a,v)
+    rob.movel([x, y, at_piece, rx, ry, rz],a,v)
+    gripper.open_gripper()
+    rob.movel([x, y, above_piece, rx', ry', rz'],a,v)
+
 
 
 if __name__ == "__main__":
@@ -113,7 +126,103 @@ if __name__ == "__main__":
         dice_num = len(keypoints)
         print(dice_num)
         cv2.waitKey(0)
+	
+	# Detect chess pieces
+    """aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+    parameters = cv2.aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(resized_down,aruco_dict,parameters=parameters)
+    out = cv2.aruco.drawDetectedMarkers(image1, corners, ids)
 
+    markerCentres = []
+    markerCenter_urx = []
+    for marker in corners:
+        centreX = (marker[0][0][0] + marker[0][2][0]) / 2
+        centreY = (marker[0][0][1] + marker[0][2][1]) / 2
+        markerCoord = []
+        markerCoord.append(centreX)
+        markerCoord.append(centreY)
+        markerCentres.append(markerCoord)
+    print(markerCentres)
+    cv2.imshow("out",out)
+    cv2.waitKey(0)"""
+
+    # Grab_the_pieces
+    """for markerTup in markerCenters:
+        pixelX = int(markerTup[0])
+        pixelY = int(markerTup[1])
+        mXcv = pixelX * 0.000264583333
+        mYcv = pixelY * 0.000264583333
+        mXcv = float("{:.4f}".format(mXcv))
+        mYcv = float("{:.4f}".format(mYcv))
+        mXurx = 0.71140 - mYcv
+        mYurx = 0.58306 - mXcv
+        markerTup[0] = mXurx
+        markerTup[1] = mYurx
+        markerCenter_urx.append(markerTup)"""
+
+    # To detect whether the dice number is odd or even
+    """
+    numAtStart = 0
+    for markerCenter in markerCenter_urx:
+        for start in startingPts:
+            if markerCenter[0] == start[0] and markerCenter[1] == start[1]:
+                numAtStart += 1
+
+    if dice_number % 2 == 1:
+        if numAtStart == 4:
+            break
+        else:
+            for step in steps:
+                if markerCenter_urx[0][0] == steps[step][0] and markerCenter_urx[0][1] == steps[step][1]:
+                    next_step_num_o = step + dice_num
+                    for nextStep in steps:
+                        if next_step_num_o == nextStep:
+                            grab_piece(markerCenter_urx[0][0], markerCenter_urx[0][1])
+                            place_piece(steps[nextStep][0], steps[nextSTep][1])
+    else:
+        if numAtStart == 4:
+            next_step_num_e == dice_num
+            for nextStep in steps:
+                if next_step_num_e == nextStep:
+                    grab_piece(markerCenter_urx[0][0],markerCenter[0][1])
+                    place_piece(steps[nextStep][0],steps[nextStep][1])
+        elif numAtStart == 3:
+            rng between (0, 1)
+            if rng == 0:
+               next_step_num_e == dice_num
+               for nextStep in steps:
+                   if next_step_num_e == nextStep:
+                      grab_piece(markerCenter_urx[1][0],markerCenter_urx[1][1])
+                      place_piece(steps[nextStep][0],steps[nextStep][1])
+            else:
+               for step in steps:
+                   if markerCenter_urx[0][0] == steps[step][0] and markerCenter_urx[0][1] == step[step][1]:
+                    next_step_num_e = step + dice_num
+                    for nextStep in steps:
+                        if next_step_num_e == nextStep:
+                            grab_piece(markerCenter_urx[0][0], markerCenter_urx[0][1])
+                            place_piece(steps[nextStep][0], steps[nextStep][1])
+        else:
+            if markerCenter_urx[0][0] != end_point[0] and markerCenter_urx[0][1] != end_point[1]:
+               for step in steps:
+                   if markerCenter_urx[0][0] == steps[step][0] and markerCenter_urx[0][1] == step[step][1]:
+                   next_step_num_e = step + dice_num
+                   for nextStep in steps:
+                       if next_step_num_e == nextStep:
+                            grab_piece(markerCenter_urx[0][0], markerCenter_urx[0][1])
+                            place_piece(steps[nextStep][0], steps[nextSTep][1])
+            else:
+                next_step_num_e == dice_num
+                for nextStep in steps:
+                if next_step_num_e == nextStep:
+                    grab_piece(markerCenter_urx[2][0],markerCenter[2][1])
+                    place_piece(steps[nextStep][0],steps[nextStep][1])
+            
+
+                
+                
+            else:
+                move the furthest piece(improve later)"""
     except:
         traceback.print_exc()
     finally:
