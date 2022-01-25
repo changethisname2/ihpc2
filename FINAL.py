09e0db759fbda1f7b0bbef5298420055b29f6a5e
changethisname2 <61044612+changethisname2@users.noreply.github.com>
2022-01-25 11:37:16 +0800

    Update FINAL.py

--- FINAL.py
+++ FINAL.py
@@ -15,8 +15,8 @@ a, v = 0.5, 0.3
 pos_dice_drop = [0.707, 0.219, 0.030, -2.205, 2.205, -0.043]
 pos_dice_grab = [0.719, 0.219, -0.200, -2.205, 2.205, -0.043]
 pos_dice_pic = [0.653, 0.187, -0.143, -2.189, 2.191, -0.011]
-pos_board = [0.647, -0.078, -0.029, -2.125, 2.190, -0.108]
-pos_boardj = [0.137, -1.985, -1.438, -1.176, 1.563, 0.166]
+pos_board = [0.685, -0.047, -0.086, -2.106, 2.029, -0.308]
+pos_boardj = [0.234, -2.031, -2.085, -0.312, 1.503, 0.184]
 
 def grab_piece(x, y):
     above_piece, rxb, ryb, rzb = -0.1502, -2.1890, 2.1920, -0.0111
@@ -72,141 +72,73 @@ if __name__ == "__main__":
                     rob.close()
                 except:
                     pass
-
-        image = cv2.imread("pre-game board.png")
-
-        # Setup SimpleBlobDetector parameters.()
-        params = cv2.SimpleBlobDetector_Params()
-
-        # Change thresholds
-        params.minThreshold = 50;
-        params.maxThreshold = 15000;
-
-        # Filter by Area.
-        params.filterByArea = True
-        params.minArea = 30
-        params.maxArea = 800
-
-        # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = 0.7
-        params.maxCircularity = 1
-
-        #Filter by Color
-        params.filterByColor = False
-
-        # Filter by Convexity
-        params.filterByConvexity = False
-
-        # Filter by Inertia
-        params.filterByInertia = False
-
-        # Create a detector with the parameters
-        ver = (cv2.__version__).split('.')
-        if int(ver[0]) < 3 :
-        	detector = cv2.SimpleBlobDetector(params)
-        else :
-        	detector = cv2.SimpleBlobDetector_create(params)
-
-
-        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        
-        # Detect blobs.
-        keypoints = detector.detect(img_gray)
-
-        coordinate = []
-        number = 1
-
-        for keypoint in keypoints:
-            pixelX = int(keypoint.pt[0])
-            pixelY = int(keypoint.pt[1])
-            mXcv = pixelX * 0.00062295
-            mYcv = pixelY * 0.00062295
-            mXcv = float("{:.4f}".format(mXcv))
-            mYcv = float("{:.4f}".format(mYcv))
-            mXurx = 0.8880 - mYcv
-            mYurx = 0.1575 - mXcv
-            cordTup = (mXurx,mYurx)
-            coordinate.append(cordTup)
-        endPts = (0.73487,-0.06897)
-
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv2.drawKeypoints(img_gray, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        # Show keypoints
-        #cv2.imshow("Keypoints", im_with_keypoints)
-        cv2.waitKey(0)
-
-        cv2.destroyAllWindows()
         
         steps = {
-            1 : coordinate [57],
-            2 : coordinate [7],
-            3 : coordinate [8],
-            4 : coordinate [27],
-            5 : coordinate [9],
-            6 : coordinate [2],
-            7 : coordinate [0],
-            8 : coordinate [23],
-            9 : coordinate [22],
-            10 : coordinate [40],
-            11 : coordinate [39],
-            12 : coordinate [19],
-            13 : coordinate [55],
-            14 : coordinate [41],
-            15 : coordinate [42],
-            16 : coordinate [43],
-            17 : coordinate [1],
-            18 : coordinate [3],
-            19 : coordinate [10],
-            20 : coordinate [26],
-            21 : coordinate [11],
-            22 : coordinate [4],
-            23 : coordinate [5],
-            24 : coordinate [56],
-            25 : coordinate [58],
-            26 : coordinate [49],
-            27 : coordinate [48],
-            28 : coordinate [35],
-            29 : coordinate [46],
-            30 : coordinate [45],
-            31 : coordinate [44],
-            32 : coordinate [59],
-            33 : coordinate [60],
-            34 : coordinate [53],
-            35 : coordinate [54],
-            36 : coordinate [37],
-            37 : coordinate [38],
-            38 : coordinate [18],
-            39 : coordinate [17],
-            40 : coordinate [16],
-            41 : coordinate [36],
-            42 : coordinate [52],
-            43 : coordinate [51],
-            44 : coordinate [50],
-            45 : coordinate [34],
-            46 : coordinate [33],
-            47 : coordinate [15],
-            48 : coordinate [14],
-            49 : coordinate [47],
-            50 : coordinate [32],
-            51 : coordinate [28],
-            52 : coordinate [29],
-            53 : coordinate [12],
-            54 : coordinate [13],
-            55 : coordinate [30],
-            56 : coordinate [31],
-            57 : endPts
+            1 : (0.7232, -0.1647),
+            2 : (0.7230, -0.1447),
+            3 : (0.7230, -0.1279),
+            4 : (0.7230, -0.1074),
+            5 : (0.7230, -0.0904),
+            6 : (0.7019, -0.0717),
+            7 : (0.6853, -0.0718),
+            8 : (0.6674, -0.0717),
+            9 : (0.6475, -0.0717),
+            10 : (0.6284, -0.0741),
+            11 : (0.6104, -0.0741),
+            12 : (0.6104, -0.0556),
+            13 : (0.6104, -0.0383),
+            14 : (0.6271, -0.0370),
+            15 : (0.6473, -0.0360),
+            16 : (0.6658, -0.0361),
+            17 : (0.6845, -0.0360),
+            18 : (0.7025, -0.-350),
+            19 : (0.7197, -0.0160),
+            20 : (0.7214, 0.0050),
+            21 : (0.7196, 0.0239),
+            22 : (0.7196, 0.0397),
+            23 : (0.7186, 0.0594),
+            24 : (0.7187, 0.0786),
+            25 : (0.7374, 0.0786),
+            26 : (0.7554, 0.0786),
+            27 : (0.7555, 0.0604),
+            28 : (0.7575, 0.0420),
+            29 : (0.7574, 0.0230),
+            30 : (0.7582, 0.0045),
+            31 : (0.7582, -0.0144),
+            32 : (0.7783, -0.0325),
+            33 : (0.7967, -0.0325),
+            34 : (0.8143, -0.0320),
+            35 : (0.8341, -0.0308),
+            36 : (0.8534, -0.0302),
+            37 : (0.8710, -0.0303),
+            38 : (0.8710, -0.0482),
+            39 : (0.8709, -0.0669),
+            40 : (0.8524, -0.0674),
+            41 : (0.8331, -0.0687),
+            42 : (0.8170, -0.0687),
+            43 : (0.7986, -0.0687),
+            44 : (0.7793, -0.0690),
+            45 : (0.7607, -0.0890),
+            46 : (0.7606, -0.1207),
+            47 : (0.7606, -0.1260),
+            48 : (0.7607, -0.1439),
+            49 : (0.7606, -0.1624),
+            50 : (0.7606,- 0.1818),
+            51 : (0.7427, -0.1818),
+            52 : (0.7439, -0.1647),
+            53 : (0.7442, -0.1458),
+            54 : (0.7445, -0.1250),
+            55 : (0.7418, -0.1065),
+            56 : (0.7420, -0.0891),
+            57 : (0.73487,-0.06897)
         }
     
         #print(steps)
         startingPts = []
-        startingPts.append(coordinate[24])
-        startingPts.append(coordinate[25])
-        startingPts.append(coordinate[21])
-        startingPts.append(coordinate[20])
+        startingPts.append((0.6792, -0.1566))
+        startingPts.append((0.6763, -0.1195))
+        startingPts.append((0.6420, -0.1213))
+        startingPts.append((0.6410, -0.1196))
 
         """Rolling dice"""
         rob.movej(pos_boardj, a, v)
@@ -290,6 +222,7 @@ if __name__ == "__main__":
 
         
         cam = cv2.VideoCapture(4)
+        gripper.open_gripper()
         rob.movel(pos_board, a, v)
         while True:
             ret, frame = cam.read()
@@ -333,12 +266,12 @@ if __name__ == "__main__":
         for markerTup in markerCenters:
             pixelX = int(markerTup[0])
             pixelY = int(markerTup[1])
-            mXcv = pixelX * 0.00062295
-            mYcv = pixelY * 0.00062295
+            mXcv = pixelX * 0.0008261
+            mYcv = pixelY * 0.0008261
             mXcv = float("{:.4f}".format(mXcv))
             mYcv = float("{:.4f}".format(mYcv))
-            mXurx = 0.8880 - mYcv
-            mYurx = 0.1575 - mXcv
+            mXurx = 0.8906 - mYcv
+            mYurx = 0.2395 - mXcv
             markerTup[0] = mXurx
             markerTup[1] = mYurx
             markerCenter_urx.append(markerTup)
